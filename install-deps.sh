defc198c85f3a795fa01b22b90e6d6ef9b39ad37
Liam Middlebrook <liammiddlebrook@gmail.com>
2015-05-04 10:06:19 -0400

    Ubuntu has weird naming conventions

--- install-deps.sh
+++ install-deps.sh
@@ -4,5 +4,5 @@ set -e -x
 VER=$TRAVIS_PYTHON_VERSION
 
 apt-get update -yqq || true
-apt-get install -yqq python$VER libxml-dev libxslt-dev
+apt-get install -yqq python$VER libxml2-dev libxslt-dev
 pip install -q requests protobuf cherrypy ws4py python-mpd2 lxml web.py --upgrade
