7493ede78c633b637892f6f8f574f2aaa7cbc203
interfect <interfect@gmail.com>
2016-03-31 22:39:49 -0700

    Test lxml vaguely after installation

--- install-deps.sh
+++ install-deps.sh
@@ -6,3 +6,7 @@ VER=$TRAVIS_PYTHON_VERSION
 apt-get update -yqq || true
 apt-get install -yqq python$VER libxml2-dev libxslt-dev
 pip install requests protobuf cherrypy ws4py python-mpd2 lxml web.py --upgrade
+
+# Make sure this works
+python -c "import lxml"
+python -c "from lxml import etree"
