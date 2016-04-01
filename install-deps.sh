4c6b85cb2b3a8f779971a458125f983f0e27d19a
interfect <interfect@gmail.com>
2016-03-31 22:29:20 -0700

    Make dependency installation noisier

--- install-deps.sh
+++ install-deps.sh
@@ -5,4 +5,4 @@ VER=$TRAVIS_PYTHON_VERSION
 
 apt-get update -yqq || true
 apt-get install -yqq python$VER libxml2-dev libxslt-dev
-pip install -q requests protobuf cherrypy ws4py python-mpd2 lxml web.py --upgrade
+pip install requests protobuf cherrypy ws4py python-mpd2 lxml web.py --upgrade
